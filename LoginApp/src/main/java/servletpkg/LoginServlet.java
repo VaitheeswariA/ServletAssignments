ea026a32debea58b36eb75bb02a2faa9e277bfa6
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-11-10 13:57:11 +0530

    Add files via upload

---
+++ LoginApp/src/main/java/servletpkg/LoginServlet.java
@@ -0,0 +1,48 @@
+package servletpkg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		PrintWriter out=response.getWriter();
+		if(username.equals("Admin") && password.equals("Admin123")) {
+			out.println("Welcome "+username);
+			out.println(LocalDateTime.now());
+		}
+		else {
+			out.print("Invalid user");
+		}
+		
+	}
+
+}
